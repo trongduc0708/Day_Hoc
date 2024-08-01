77de44ec0f420cfde6ee3a7af45383a760c20f00
LXC1-2-3 <bomezoic@gmail.com>
2024-08-02 00:30:43 +0700

    foder bai tap java

---
+++ bai_tap_java_core/src/bai_tap_java_core/Bai_tap_java_core.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bai_tap_java_core;
+
+/**
+ *
+ * @author luong
+ */
+public class Bai_tap_java_core {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
