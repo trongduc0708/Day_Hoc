1f84cb9d44e16cf9482513c18af9ad2b600e23db
LXC1-2-3 <bomezoic@gmail.com>
2024-08-02 01:08:38 +0700

    bai 2

--- bai_tap_java_core/src/bai_tap_java_core/Bai_tap_java_core.java
+++ bai_tap_java_core/src/bai_tap_java_core/Bai_tap_java_core.java
@@ -5,6 +5,8 @@
  */
 package bai_tap_java_core;
 
+import java.util.Scanner;
+
 /**
  *
  * @author luong
@@ -14,8 +16,44 @@ public class Bai_tap_java_core {
     /**
      * @param args the command line arguments
      */
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("nhap so bat ki: ");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        switch (n) {
+            case 1:
+                System.out.println("một");
+                break;
+            case 2:
+                System.out.println("hai");
+                break;
+            case 3:
+                System.out.println("ba");
+                break;
+            case 4:
+                System.out.println("bốn");
+                break;
+            case 5:
+                System.out.println("năm");
+                break;
+            case 6:
+                System.out.println("sáu");
+                break;
+            case 7:
+                System.out.println("bảy");
+                break;
+            case 8:
+                System.out.println("tám");
+                break;
+            case 9:
+                System.out.println("chín");
+                break;
+            case 10:
+                System.out.println("mười");
+                break;
+        }
+
     }
-    
+
 }
